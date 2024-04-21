b8757ec7303439aa3eda3679ceb02b3a86bb0bce
DoronTavor <tavor.doron@gmail.com>
2024-04-21 14:36:25 +0300

    Lesson 48- The third part of list methods

---
+++ Section3/lesson48.py
@@ -0,0 +1,9 @@
+# List Methods
+basket = ['x','a','b','c','d','e','d']
+basket.sort()
+print(basket)
+
+basket = ['x','a','b','c','d','e','d']
+print(sorted(basket))
+basket.reverse()
+print(basket)
