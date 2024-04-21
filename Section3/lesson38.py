9c62088f8417f2abd8375d7e8494d2c99578184c
DoronTavor <tavor.doron@gmail.com>
2024-04-21 11:59:07 +0300

    Lesson 38- Built in functions and methods of strings

---
+++ Section3/lesson38.py
@@ -0,0 +1,12 @@
+# BUILT IN FUNCTIONS + METHODS
+print(len("abcdefghijklmnopqrstuvwxyz"))
+greet= "hellloooo"
+print(greet[0:len(greet)])
+
+quote= "to be or not to be"
+print(quote.upper())
+print(quote.capitalize())
+print(quote.lower())
+print(quote.find("be"))
+print(quote.replace("be","me"))
+print(quote)
