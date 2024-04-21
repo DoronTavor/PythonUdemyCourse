9f141deeef7332502fa1b31c017a24aca487a210
DoronTavor <tavor.doron@gmail.com>
2024-04-21 14:28:15 +0300

    Lesson 42+26- Exercise + first part of list methods

---
+++ Section3/lesson42.py
@@ -0,0 +1,5 @@
+# Password checker
+username= input("Enter the username: ")
+password= input("Enter your password: ")
+
+print(f"{username},your password {'*'*len(password)} is: {len(password)} letters long")
---
+++ Section3/lesson46.py
@@ -0,0 +1,19 @@
+# List Methods
+basket = [1,2,3,4,5]
+print(len(basket))
+
+basket.append(100)
+print(basket)
+basket.insert(4,200)
+print(basket)
+
+basket.extend([100,101])
+print(basket)
+
+# REMOVING
+basket.pop()
+print(basket)
+basket.pop(0)
+print(basket)
+basket.remove(200)
+print(basket)
