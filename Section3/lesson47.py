e54f8d48196fd4eec35df3db68b77a151fa6c84f
DoronTavor <tavor.doron@gmail.com>
2024-04-21 14:32:42 +0300

    Lesson 47- The second part of list methods

---
+++ Section3/lesson47.py
@@ -0,0 +1,10 @@
+# List Methods
+basket = ['a','b','c','d','e']
+
+print(basket.index('d'))
+
+# print(basket.index('d',0,2)) # Gives an error
+
+print('d' in basket)
+print('x' in basket)
+print(basket.count('d'))
