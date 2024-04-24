6820992955a10f5cab492ae7c5abcbcde86d65b2
DoronTavor <tavor.doron@gmail.com>
2024-04-24 13:04:27 +0300

    Lesson 144- zip

---
+++ Section7/Lesson144.py
@@ -0,0 +1,10 @@
+# zip()
+my_lst=[1,2,3]
+your_lst=[10,20,30]
+print(list(zip(my_lst,your_lst)))
+
+your_tpl= (10,20,30)
+print(list(zip(my_lst,your_tpl)))
+
+their_lst=(5,4,3)
+print(list(zip(my_lst,your_tpl,their_lst)))
